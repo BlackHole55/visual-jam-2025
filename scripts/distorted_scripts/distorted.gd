d938c6a75de9938d67ddf9b21cd546bca613ecc4
fallendwn <denislee56@gmail.com>
2025-04-26 23:46:33 +0500

    база

---
+++ scripts/distorted_scripts/distorted.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+@export var speed = 130
+@onready var sprite = $animation_of_character_distorted
+var jump_velocity = -300
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _ready() -> void:
+	position = PositionTracking.player_position
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("distortion"):
+		PositionTracking.player_position = $".".position
+		get_tree().change_scene_to_file("res://scenes/first_normal_scene/first_location_normal.tscn")
+		
+
+func _physics_process(delta):
+	var direction = Input.get_axis("left","right")
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+		
+	
+	
+	
+	if direction > 0:
+		sprite.flip_h = false
+	elif direction < 0:
+		sprite.flip_h = true
+	
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		
+	move_and_slide()
+
+	
+	

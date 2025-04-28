c5cffcb54848cd315bcc57feeeba40b6d0fb7493
fallendwn <denislee56@gmail.com>
2025-04-28 20:37:42 +0500

    feat: running animation, pick_up function (not finished)

--- scripts/distorted_scripts/distorted.gd
+++ scripts/distorted_scripts/distorted.gd
@@ -5,24 +5,43 @@ extends CharacterBody2D
 @export var Coyote_Time : float = 0.1
 @onready var sprite = $animation_of_character_distorted
 
-
 var jump_velocity = -300
 var Jump_Buffer:bool = false
 var Jump_Available : bool = true
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var SpawnPoint = Vector2.ZERO
+#Instantly replace player with global variable position (defaul zero vector, but after 
+#distortion input it replaces zero vector with coordinates on another dimension)
+#also checks if character after swap is in the wall, it changes position to spawnpoint!!
 
-
-func _ready() -> void:
+func _ready():
 	position = PositionTracking.player_position
 	
+	await get_tree().process_frame 
+	
+	if is_stuck():
+		position = SpawnPoint
+
+#switches the realms
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("distortion"):
 		PositionTracking.player_position = $".".position
 		get_tree().change_scene_to_file("res://scenes/first_normal_scene/first_location_normal.tscn")
+	
+	#sprint
+	if event.is_action_pressed("sprint"):
+		speed = 180
 		
+	if event.is_action_released("sprint"):
+		speed = 130
 		
 func _physics_process(delta):
+	
+	
+	#gets direction (either negative either positive number)
 	var direction = Input.get_axis("left","right")
+	#Check if character is on floor, else you can jump
+	#also has jump_buffer and coyote_time feature;
 	if not is_on_floor():
 		if Jump_Available:
 			get_tree().create_timer(Coyote_Time).timeout.connect(Coyote_Timeout)
@@ -33,33 +52,41 @@ func _physics_process(delta):
 			Jump()
 			Jump_Buffer = false
 	
+	#Jump function, which tracks when the button was pressed
 	if Input.is_action_just_pressed("jump"):
 		if Jump_Available:
 			Jump()
 		else:
 			Jump_Buffer = true
 			get_tree().create_timer(Jump_Buffer_Time).timeout.connect(on_jump_buffer_timeout)
-		
+	
+	#if direction is negative, it flips the character, so it looks to the left
 	if direction > 0:
 		sprite.flip_h = false
 	elif direction < 0:
 		sprite.flip_h = true
 	
+	#If direction has a value, it means player pressed move key
+	#so this function moves character in game
 	if direction:
 		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 
-		
+	#last function, which glue everything together.
 	move_and_slide()
-		
+	
 func Coyote_Timeout():
 	Jump_Available = false
-	
+
 func Jump():
 	velocity.y = jump_velocity
 	Jump_Available = false
 
 func on_jump_buffer_timeout():
 	Jump_Buffer = false
+	
+func is_stuck() -> bool:
+	var collision = move_and_collide(Vector2.ZERO)
+	return collision != null
---
+++ scripts/global/variables.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var is_Pressed : bool = false
